1dc506d144930e966590a264b0421e2ee6743286
truvem <gettruvem@gmail.com>
2026-06-20 10:56:07 +0000

    Bump version from 0.1.0 to 0.1.1

--- sdk/setup.py
+++ sdk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="truvem",
-    version="0.1.0",
+    version="0.1.1",
     description="Universal persistent memory for AI agents",
     author="Truvem",
     author_email="gettruvem@gmail.com",
